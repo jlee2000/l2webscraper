233087e1d48653911758eae50e87cafb311a6e28
Jiho Lee <jiho.lee2000@gmail.com>
2022-06-07 09:56:52 -0400

    add option to hand in more links as command line arguments

--- l2webscraper/l2webscraper.py
+++ l2webscraper/l2webscraper.py
@@ -1,7 +1,15 @@
 import requests
 import pandas as pd
+import sys
 from io import BytesIO
+
 urls = ['https://optimistic.etherscan.io/chart/verified-contracts?output=csv', 'https://arbiscan.io/chart/verified-contracts?output=csv', 'https://polygonscan.com/chart/verified-contracts?output=csv']
+
+# to use command-line arguments, use the following format:
+#   python3 l2webscraper.py "URL_1.xyz" "URL_2.com" "URL_3.org" ...
+for i in range(1, len(sys.argv)):
+    urls.append(sys.argv[i])
+
 header = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
     'referer':'https://www.google.com/'
